b6591caac2036a73a488b3f3fcd1a5c986112472
ThijsGeeraert2 <thijs.geeraert@student.vives.be>
2024-02-20 09:34:47 +0100

    Initial commit

---
+++ app/src/main/java/be/thijsgeeraert/studdybuddy/MainActivity.kt
@@ -0,0 +1,46 @@
+package be.thijsgeeraert.studdybuddy
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import be.thijsgeeraert.studdybuddy.ui.theme.StuddybuddyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            StuddybuddyTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    StuddybuddyTheme {
+        Greeting("Android")
+    }
+}
