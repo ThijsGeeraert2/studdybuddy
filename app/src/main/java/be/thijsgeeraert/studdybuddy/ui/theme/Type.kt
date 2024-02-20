10240da918efbb4934bcd320faf7e7113868c86f
ThijsGeeraert2 <thijs.geeraert@student.vives.be>
2024-02-20 11:35:47 +0100

    font

--- app/src/main/java/be/thijsgeeraert/studdybuddy/ui/theme/Type.kt
+++ app/src/main/java/be/thijsgeeraert/studdybuddy/ui/theme/Type.kt
@@ -2,14 +2,20 @@ package be.thijsgeeraert.studdybuddy.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import be.thijsgeeraert.studdybuddy.R
 
+
+val Poppins = FontFamily(
+    Font(R.font.poppins_regular)
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Poppins,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
